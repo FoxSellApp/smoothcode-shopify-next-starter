4678866db58a74b46b8dc72354829948434b6b05
dev-prakhar <prakhar@pushowl.com>
2022-08-08 16:05:15 +0530

    Added SmoothCode Starter Code

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,33 @@
-import '../styles/globals.css'
+import {AppProvider} from "@shopify/polaris";
+import App from "next/app";
+import enTranslations from "@shopify/polaris/locales/en.json"
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import '../styles/globals.css'
+import '@shopify/polaris/build/esm/styles.css';
+
+function MyApp({Component, pageProps}) {
+    return (
+        <AppProvider i18n={enTranslations}>
+            <Component {...pageProps} />
+        </AppProvider>
+    )
 }
+
+MyApp.getInitialProps = async (appContext) => {
+    const appProps = await App.getInitialProps(appContext)
+
+    const ctx = appContext.ctx
+    const shop = ctx.query.shop
+
+    ctx.res.setHeader(
+        'Content-Security-Policy', `frame-ancestors https://${shop} https://admin.shopify.com/`
+    )
+    ctx.res.setHeader(
+        'X-Frame-Options', 'ALLOWALL'
+    )
+
+    return {...appProps}
+}
+
 
 export default MyApp
